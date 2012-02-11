0caef28e6f7020a7e5f2fae2ae7981f4c3931feb
rickyd493@gmail.com <rickyd493@gmail.com@5711d5e0-011c-1b79-170f-bd2b7e18747d>
2012-02-11 13:43:16 +0000

    Finished searching using both depth first and breadth first methods. Added state class to act as wrapper for the state and the number of iterations.

--- trunk/MIU/src/main.java
+++ trunk/MIU/src/main.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
+import java.util.TreeSet;
 
 
 
 public class main {
 
+    private static final int ITERATIONS = 7;
     private static String input;
     private static char[] miu;
 
@@ -12,61 +14,159 @@ public class main {
 
     public static void main(String[] args)
     {
+	
+	breadthSearch("MIIIUU", "MUUU");
+	depthSearch("MIIIUU", "MUUU");
+	
+	
+    }
+
+    public static TreeSet<State> getOutputs(State s)
+    {
 
 	input = new String();
-	input = "MIUU";
+	input = s.getMiu();
 
 	input.toUpperCase();
-	ArrayList<String> outputs = new ArrayList<String>();
+	TreeSet<State> outputs = new TreeSet<State>();
 
-
-	outputs.add(input + input.substring(1));
-
+	
 	if(input.substring(input.length()-1).equals("I"))
 
 	{
 
-	    outputs.add(input+"U");
+	    outputs.add(new State( s.getLength()+1, input + input+"U"));
 
 	}
 
+	outputs.add(new State( s.getLength()+1, input + input.substring(1)));
 
+	
 	for (int i = 1; i< input.length(); i++ )
 	{
 
-
-	    if ( i == input.length()-2 && input.substring(i, i+2).equals("UU"))
-	    {
-
-		System.out.println("uu 1");
-		outputs.add(input.substring(0, i));
-
-	    }
-
-	    if ( i < input.length()-2 && input.substring(i, i+2).equals("UU"))
-	    {
-
-		outputs.add(input.substring(0, i)+ input.substring(i+2));
-
-	    }
-
+	    
 	    if((i == (input.length()-3)) && input.substring(i, i+3).equals("III"))
 	    {
 
-		outputs.add(input.substring(0, i) + "U");
+		outputs.add(new State(s.getLength()+1, input.substring(0, i) + "U"));
 
 	    }
 
 	    if ( i < input.length()-3 && input.substring(i, i+3).equals("III"))
 	    {
 
-		outputs.add(input.substring(0, i)+ "U" +  input.substring(i+3));
+		outputs.add(new State(s.getLength()+1, input.substring(0, i)+ "U" +  input.substring(i+3)));
 
 	    }
+
+	    if ( i == input.length()-2 && input.substring(i, i+2).equals("UU"))
+	    {
+
+		outputs.add(new State(s.getLength()+1,input.substring(0, i)));
+
+	    }
+
+	    if ( i < input.length()-2 && input.substring(i, i+2).equals("UU"))
+	    {
+
+		outputs.add(new State(s.getLength()+1, input.substring(0, i)+ input.substring(i+2)));
+
+	    }
+
+	
 	}
 
-	System.out.println(outputs.toString());
+	return outputs;
     }
+
+    public static void depthSearch(String src, String dest){
+
+	ArrayList<State> agenda = new ArrayList<State>();
+	TreeSet<String> closed = new TreeSet<String>();
+	State current = new State(src);
+	agenda.add(current);
+
+	long time = System.nanoTime();
+	while (agenda.size() != 0)
+	{
+
+	    current = agenda.get(agenda.size()-1);
+	    agenda.remove(current);
+	    
+	    if(current.getMiu().equals(dest))
+	    {
+		
+		time = System.nanoTime() - time;
+		System.out.println("Found it! " + current.getMiu() + " was found " + current.getLength() + " iteration(s) into the tree!");
+		System.out.println("Took " + time + " nanoseconds to find using depth first search");
+		return;
+
+	    }
+	    else
+	    {
+		if(!closed.contains(current.getMiu()) && current.getLength() < ITERATIONS) //to stop it from iterating too deep into the tree
+		{
+
+		    TreeSet<State> next = new TreeSet<State>();
+		    next = getOutputs(current);
+		    closed.add(current.getMiu());
+		    agenda.addAll(next);
+
+		}
+	    }
+	}
+	
+	time = System.nanoTime() - time;
+	System.out.println(dest + " could not be found!");
+	System.out.println("Took " + time + " nanoseconds to unsuccessfully find the string within " + ITERATIONS + " iterations using depth first search");
+	
+    }
+
+    public static void breadthSearch(String src, String dest){
+
+	ArrayList<State> agenda = new ArrayList<State>();
+	TreeSet<String> closed = new TreeSet<String>();
+	State current = new State(src);
+	agenda.add(current);
+
+	long time = System.nanoTime();
+	while (agenda.size() != 0)
+	{
+
+	    current = agenda.get(0);
+	    agenda.remove(0);
+	    
+	    if(current.getMiu().equals(dest))
+	    {
+		
+		time = System.nanoTime() - time;
+		System.out.println("Found it! " + current.getMiu() + " was found " + current.getLength() + " iteration(s) into the tree!");
+		System.out.println("Took " + time + " nanoseconds to find using breadth first search");
+		return;
+
+	    }
+	    else
+	    {
+		if(!closed.contains(current.getMiu()) && current.getLength() < ITERATIONS) //to stop it from iterating too deep into the tree
+		{
+
+		    TreeSet<State> next = new TreeSet<State>();
+		    next = getOutputs(current);
+		    closed.add(current.getMiu());
+		    agenda.addAll(next);
+
+		}
+	    }
+	}
+	
+	time = System.nanoTime() - time;
+	System.out.println(dest + " could not be found!");
+	System.out.println("Took " + time + " nanoseconds to unsuccessfully find the string within " + ITERATIONS + " iterations using breadth first search");
+	
+    }
+
+    
 }
 
 
